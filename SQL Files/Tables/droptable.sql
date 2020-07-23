fe6fd4a0f116d6cddd7e6130ee9828db27d1cf3f
Nabin Atreya <30519755+Shadymania@users.noreply.github.com>
2020-07-23 18:07:59 +0545

    Add files via upload
    Admin SQL Files (Oracle Apex)

---
+++ SQL Files/Tables/droptable.sql
@@ -0,0 +1,32 @@
+drop table collection_slot cascade constraints;
+
+drop table payment cascade constraints;
+
+drop table discount cascade constraints;
+
+drop table cart cascade constraints;
+
+drop table orders cascade constraints;
+
+drop table review cascade constraints;
+
+drop table product cascade constraints;
+
+drop table product_type cascade constraints;
+
+drop table shop cascade constraints;
+
+drop table trader cascade constraints;
+
+drop table users cascade constraints;
+
+
+
+
+
+
+
+
+
+
+
